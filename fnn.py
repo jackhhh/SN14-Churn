8f89bced16ec80408d36a6aebaf92f8d818bb23d
Jack <me@jackh.cn>
2021-05-28 20:44:40 +0800

    Bug fixed and results added.

--- fnn.py
+++ fnn.py
@@ -9,6 +9,38 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import seaborn as sn
+# Function to plot the confusion matrix (code from https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html)
+from sklearn import metrics
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 
 data = pd.read_json('../yelp_dataset/yelp_dataset_labeled.json', lines=True).fillna(0)
@@ -75,12 +107,13 @@ model.evaluate(X_test, y_test)
 y_pred_prob = model.predict(X_test)
 y_pred = np.argmax(model.predict(X_test), axis=-1)
 
+cm = metrics.confusion_matrix(y_test, y_pred)
 conf_matrix = tf.math.confusion_matrix(y_test, y_pred, num_classes=2).numpy()
 classes = [0, 1]
 conf_df = pd.DataFrame(conf_matrix, index=classes ,columns=classes)
 plt.subplots(figsize=(12, 9))
 conf_fig = sn.heatmap(conf_df, annot=True, fmt="d", cmap="BuPu")
 plt.show()
-
+plot_confusion_matrix(cm, classes=['Fake', 'Real'])
 print(classification_report(y_test, y_pred))
 print('ml train model auc score {:.6f}'.format(roc_auc_score(y_test, y_pred_prob[:,1])))
